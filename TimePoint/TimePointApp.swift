e3cfe0b750c7fd988ccf5e3fa208ac3d56ca73a1
jaehyeon(wiz) <eiirwp@gmail.com>
2021-12-11 22:16:06 +0900

    Initial Commit

---
+++ TimePoint/TimePointApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimePointApp.swift
+//  TimePoint
+//
+//  Created by wiz on 2021/12/11.
+//
+
+import SwiftUI
+
+@main
+struct TimePointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
